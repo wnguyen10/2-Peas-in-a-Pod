d039343e33d39237eeb732a58d590491aad80ce6
rcl228 <rcl228@cornell.edu>
2023-03-26 16:39:43 -0400

    design changes

--- podcast_app/src/pages/Home.js
+++ podcast_app/src/pages/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import User from '../components/User'
 import "./Home.css"
+import logo from "../data/logo.png"
 
 function Home() {
   return (
     <div className='Home'>
-      <h1 className='Title'>
-        2 Peas in a Pod
-      </h1>
+      <div className='Title'>
+        <img src={logo} width="400" height="300" />
+        <text> 2 PEAS IN A POD</text>
+      </div>
+      <div>  <p> Input your favorite stations and find your perfect blend:</p></div>
       <div className='Users'>
         <User num={1} />
         <User num={2} />
+      </div>
+      <div>
+        <button type="button">Find Matches!</button>
       </div>
     </div>
   )
