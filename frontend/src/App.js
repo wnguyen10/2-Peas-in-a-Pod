538db4daa8f69b756760c7f31298e6131b052f9c
rcl228 <rcl228@cornell.edu>
2023-03-27 18:07:24 -0400

    move everything to front end and added build folder

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
